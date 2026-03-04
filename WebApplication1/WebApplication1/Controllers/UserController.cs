40fc89d35a23ad30f6d59d4a600e1c34a7352c6f
Jan Horák <horakj24@spstrutnov.cz>
2026-03-04 11:17:32 +0100

    Revert "předělávky"
    This reverts commit 95fd81444d032a0d8307a8bb921723c482ac6f9d.

--- WebApplication1/WebApplication1/Controllers/UserController.cs
+++ WebApplication1/WebApplication1/Controllers/UserController.cs
@@ -4,35 +4,14 @@ namespace WebApplication1.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult Register()
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Register(string username, string email, string password, string confirmPassword)
-        {
-            // Zde by byla validace a uložení do databáze
-            TempData["SuccessMessage"] = "Úspěšně registrováno";
-            return RedirectToAction("Index");
-        }
-
         public IActionResult Login()
         {
             return View();
-        }
-
-        [HttpPost]
-        public IActionResult Login(string email, string password, bool rememberMe)
-        {
-            // Zde by byla autentifikace
-            TempData["SuccessMessage"] = "Úspěšně přihlášeno";
-            return RedirectToAction("Index");
         }
 
         public IActionResult Profile()
