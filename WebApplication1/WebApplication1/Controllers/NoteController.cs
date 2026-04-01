4f7f4514f8c2407d11411faf412164183234c634
Jan Horák <horakj24@spstrutnov.cz>
2026-04-01 11:17:19 +0200

    model a vyplnění těch věcí

--- WebApplication1/WebApplication1/Controllers/NoteController.cs
+++ WebApplication1/WebApplication1/Controllers/NoteController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class NoteController : Controller
     {
-        public IActionResult Index()
+        // Dočasné úložiště (bez databáze)
+        private static List<Note> _notes = new();
+        private static int _nextId = 1;
+
+        // GET /Note/List
+        public IActionResult List()
+        {
+            return View(_notes);
+        }
+
+        // GET /Note/Add
+        public IActionResult Add()
         {
             return View();
+        }
+
+        // POST /Note/Add
+        [HttpPost]
+        public IActionResult Add(Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                note.Id = _nextId++;
+                note.CreatedAt = DateTime.Now;
+                _notes.Add(note);
+                return RedirectToAction("List");
+            }
+            return View(note);
         }
     }
 }
